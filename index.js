63ebbe84f6992496f4f14d5394edfebccb5232c6
Michael Bumann <hello@michaelbumann.com>
2020-05-29 01:31:41 +0200

    Export expense schema

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const path = require('path');
 
 module.exports = {
   "contribution": JSON.parse(fs.readFileSync(path.join(__dirname, 'schemas', 'contribution.json'))),
-  "contributor": JSON.parse(fs.readFileSync(path.join(__dirname, 'schemas', 'contributor.json')))
+  "contributor": JSON.parse(fs.readFileSync(path.join(__dirname, 'schemas', 'contributor.json'))),
+  "expense": JSON.parse(fs.readFileSync(path.join(__dirname, 'schemas', 'expense.json')))
 };
